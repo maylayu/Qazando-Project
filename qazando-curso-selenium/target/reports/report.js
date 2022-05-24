1870cd2d99db62514ae961705bcf77401b2ec6e7
maylayu <mayla.amaral@fatec.sp.gov.br>
2022-05-24 09:01:48 -0300

    80%

--- qazando-curso-selenium/target/reports/report.js
+++ qazando-curso-selenium/target/reports/report.js
@@ -61,35 +61,35 @@ formatter.match({
   "location": "LoginSteps.que_estou_na_tela_de_login()"
 });
 formatter.result({
-  "duration": 4159628600,
+  "duration": 3768964100,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.acesso_o_cadastro_de_usuario()"
 });
 formatter.result({
-  "duration": 250778500,
+  "duration": 350947200,
   "status": "passed"
 });
 formatter.match({
   "location": "CadastroSteps.preencho_o_formulario_de_cadastro()"
 });
 formatter.result({
-  "duration": 6008658300,
+  "duration": 5729812600,
   "status": "passed"
 });
 formatter.match({
   "location": "CadastroSteps.clico_em_registrar()"
 });
 formatter.result({
-  "duration": 51800,
+  "duration": 29400,
   "status": "passed"
 });
 formatter.match({
   "location": "CadastroSteps.vejo_a_mensagem_de_cadastro_realizado_com_sucesso()"
 });
 formatter.result({
-  "duration": 32800,
+  "duration": 25200,
   "status": "passed"
 });
 });
